5ef2f076cbaad0d031a02cef3d77a2487379e433
Jaac <jaac219@gmail.com>
2022-02-08 05:52:29 -0500

    revision final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -237,7 +237,7 @@ function esVocal(letra){
   //Escribe tu código aquí
 
   if(letra.length != 1)return 'Dato incorrecto';
-  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
       return 'Es vocal';
   }else{
       return 'Dato incorrecto';
